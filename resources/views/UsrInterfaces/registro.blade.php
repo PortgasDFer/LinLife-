28544bf280652df2839c813f73dcec43a5cb0481
PortgasDFer <fernando_ls96@hotmail.com>
2021-04-17 11:09:45 -0500

    Integración API Sepomex

--- resources/views/UsrInterfaces/registro.blade.php
+++ resources/views/UsrInterfaces/registro.blade.php
@@ -224,9 +224,14 @@
           console.log(data);
           var string = JSON.stringify(data)
           var datos = JSON.parse(string);
-          console.log(datos[0].response.estado);
           document.registro.localidad.value=datos[0].response.municipio;
           document.getElementById("entidad").value=datos[0].response.estado;
+          $('#colonia').empty();
+          $('#colonia').append('<option value="" class="form-control" disable="true" selected="true">Seleccione una colonia</option>');
+          $.each(datos,function(fetch, colonia){
+            //console.log(colonia.response.asentamiento);
+            $('#colonia').append('<option class="form-control" value="'+colonia.response.asentamiento+'" disable="true" selected="true">'+colonia.response.asentamiento+'</option>');
+          })
       })
     }
   </script>
