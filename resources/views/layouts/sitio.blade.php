aba4533846e1045f127db6308b93902d5b7e00ac
daniel29isai <41874620+daniel29isai@users.noreply.github.com>
2021-05-08 17:06:40 -0500

    Update sitio.blade.php

--- resources/views/layouts/sitio.blade.php
+++ resources/views/layouts/sitio.blade.php
@@ -22,7 +22,7 @@
 		    <div class="main_menu">
 		      <nav class="navbar navbar-expand-lg navbar-light">
 		        <div class="container">
-		          <a class="navbar-brand logo_h" href="index.html"><img src="/recursos/linlife.png" alt="" width="120"></a>
+		          <a class="navbar-brand logo_h" href="/"><img src="/recursos/linlife.png" alt="" width="120"></a>
 		          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 		            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		            <span class="icon-bar"></span>
