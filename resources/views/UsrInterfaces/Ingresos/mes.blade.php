badc5c57f19838fe51119345fc8472815534a29e
daniel29isai <41874620+daniel29isai@users.noreply.github.com>
2021-06-03 16:42:23 -0500

    Update mes.blade.php

--- resources/views/UsrInterfaces/Ingresos/mes.blade.php
+++ resources/views/UsrInterfaces/Ingresos/mes.blade.php
@@ -24,7 +24,15 @@
        <div class="card">
               <div class="card-header border-0">
                 <div class="d-flex justify-content-between">
-                  <h3 class="card-title">Ventas</h3>                  
+                  <h3 class="card-title">Ventas</h3>
+                  <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>                  
                 </div>
               </div>
               <div class="card-body">
